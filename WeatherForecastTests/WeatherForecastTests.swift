3dc46b42e16d08c3438f5adbf0e784bb8114bb4d
Fahad <fahad@macs-mac-mini.local>
2019-04-25 18:47:52 +0600

    initial upload

--- WeatherForecastTests/WeatherForecastTests.swift
+++ WeatherForecastTests/WeatherForecastTests.swift
@@ -10,25 +10,111 @@ import XCTest
 @testable import WeatherForecast
 
 class WeatherForecastTests: XCTestCase {
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    
+    func testWeatherDataModel(){
+        
+        var weatherDatas: WeatherData!
+        
+        do {
+            if let file = Bundle.main.url(forResource: "Dummy", withExtension: "json") {
+                
+                let data = try Data(contentsOf: file)
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let object = json as? [String: Any] {
+                    weatherDatas = WeatherData(jsonData: object)
+                }
+            }
+        } catch {
+            XCTFail("Failed")
+            print(error.localizedDescription)
         }
+
+        XCTAssertEqual(weatherDatas.summary, "__")
+        XCTAssertEqual(weatherDatas.temperature, "53 ºF")
+        XCTAssertEqual(weatherDatas.icon, "__")
+  
+    }
+    
+    func testWeatherService(){
+
+        let promise = expectation(description: "Status code: 200")
+        var longitude = "0"
+        var latitude  = "0"
+        
+        var weatherService = WeatherService()
+        
+        var dummyData : WeatherData!
+        
+        
+        
+        
+        weatherService.weatherForCoordinates(longitute: longitude, latitude: latitude) { (WeatherData, error) in
+            
+            if let weatherdata = WeatherData{
+               dummyData = weatherdata
+               print(weatherdata)
+               XCTAssertEqual(dummyData.temperature, "84 ºF")
+               promise.fulfill()
+            }
+            else if let error = error{
+                print("Error")
+                XCTAssertNil(WeatherData)
+                promise.fulfill()
+            
+            }
+        }
+       waitForExpectations(timeout: 4, handler: nil)
+       
+ 
+        }
+    
+    func testWeatherServiceError(){
+        
+        
+        let promise = expectation(description: "Status code: 200")
+        var longitude = "_"
+        var latitude  = "_"
+        
+        var weatherService = WeatherService()
+        
+        var dummyData : WeatherData!
+        
+        weatherService.weatherForCoordinates(longitute: longitude, latitude: latitude) { (WeatherData, error) in
+            
+            if let weatherdata = WeatherData{
+                dummyData = weatherdata
+                print(weatherdata)
+                XCTAssertEqual(dummyData.temperature, "84 ºF")
+                promise.fulfill()
+            }
+            else if let error = error{
+                print("Error")
+                XCTAssertNil(WeatherData)
+                promise.fulfill()
+                
+            }
+        }
+        waitForExpectations(timeout: 4, handler: nil)  
+        
     }
 
-}
+    
+   }
+        
+        
+        
+
+    
+    
+
